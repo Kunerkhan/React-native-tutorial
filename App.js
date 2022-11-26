3f99200d53dc3db23d0609445813b7ce647aaaac
kunerkhan.akerov <kunerkhan.akerov@yellow.systems>
2022-11-27 00:08:13 +0100

    Introduce view and text components

--- App.js
+++ App.js
@@ -4,8 +4,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>
+          Hello, World!
+        </Text>
+      </View>
+
+      <View style={styles.body}>
+        <Text>
+           Lorem ipsum dolor sit amet.
+        </Text>
+        <Text>
+           Lorem ipsum dolor sit amet.
+        </Text>
+        <Text>
+           Lorem ipsum dolor sit amet.
+        </Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +32,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
+  }
 });
